a5f07e8809522734fc869a8e59e2d7d92b78832b
vinitkp <vinit.kp@live.in>
2013-05-04 08:05:58 +0530

    changes made

--- InternalizationSupportProg.java
+++ InternalizationSupportProg.java
@@ -3,12 +3,14 @@ class InternationalizationSupportProg{
 	public InternationalizationSupportProg(){
 		String language;
 		String country;
-		language = new String("hi");
+		language = new String("en");
 		country = new String("US");
 		Locale currentLocale;
 		ResourceBundle messages;
 		currentLocale = new Locale(language, country);
-		messages = ResourceBundle.getBundle("for_use_poc-martus_random-strings",currentLocale);
+		System.out.println(currentLocale);
+		messages = ResourceBundle.getBundle("for_use_poc-martus_random-strings");
+		System.out.println(messages.toString());
 		System.out.println(messages.getString("building"));
 	}
 	public static void main(String []args){
